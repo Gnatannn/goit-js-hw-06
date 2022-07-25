ff401f8074a93934c955a67877f69a9f74c34b1e
Gnatannn <96788997+Gnatannn@users.noreply.github.com>
2022-07-25 23:50:35 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -10,39 +10,27 @@ const refs = {
   boxes: document.querySelector("#boxes"),
 };
 
-// console.log(refs.createBtn);
-// console.log(refs.destroyBtn);
-
-let inputNumber = 0;
-
-function onClickInput(event) {
-  inputNumber = Number(event.currentTarget.value);
+refs.createBtn.addEventListener("click", () => {
+  const inputNumber = Number(refs.input.value);
   console.log(inputNumber);
-}
+  createBoxes(inputNumber);
+  refs.input.value = "";
+});
+refs.destroyBtn.addEventListener("click", destroyBoxes);
 
 function createBoxes(amount) {
+  refs.boxes.innerHTML = "";
   let divArr = [];
-  // for (let i = 0; i < 6; i += 1) {
-  //   divArr.push(
-  //     `<div width="30px" height="30px" backgroud-color="${getRandomHexColor()}">a</div)`
-  //   );
-  // }
 
-  for (let i = 0; i < 6; i += 1) {
+  for (let i = 0; i < amount; i += 1) {
     const initialSize = 30;
     let addSize = initialSize + i * 10;
     const boxItem = `<div style="width:${addSize}px;
     height:${addSize}px; background-color:${getRandomHexColor()}" ></div>`;
     divArr.push(boxItem);
   }
-
-  // console.log(divArr);
   boxes.insertAdjacentHTML("afterbegin", divArr.join(""));
 }
-
-refs.input.addEventListener("input", onClickInput);
-refs.createBtn.addEventListener("click", createBoxes);
-refs.destroyBtn.addEventListener("click", destroyBoxes);
 
 function destroyBoxes() {
   refs.boxes.innerHTML = "";
